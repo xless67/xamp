c62c1fedba6eef67cc2330f6ebd3ce61c5ebc69a
Aleksey <a.tereschenko@avalonfundaktiv.com>
2017-02-24 13:55:05 +0200

    init commit

---
+++ app/src/main/java/com/xless/xamp/MediaLibrary.java
@@ -0,0 +1,13 @@
+package com.xless.xamp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MediaLibrary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_library);
+    }
+}
